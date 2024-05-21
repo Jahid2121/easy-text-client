f21ea993875b10a09a41d14e333c1a2cd66cc574
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-05-21 20:01:59 +0600

    Use tanstack query to load rooms data

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -1,34 +1,24 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import useChatRooms from '@/hooks/UseChatRooms';
 
 const ChatRoomPage = () => {
-    const [chatRooms, setChatRooms] = useState([]);
-
-    useEffect(() => {
-        const fetchChatRooms = async () => {
-            try {
-                const response = await fetch('http://localhost:5000/rooms');
-                const data = await response.json();
-                setChatRooms(data.data);
-            } catch (error) {
-                console.error('Error fetching chat rooms:', error);
-            }
-        };
-
-        fetchChatRooms();
-    }, []);
+    const [rooms] = useChatRooms()
+    const chatRooms = rooms.data
+    console.log(chatRooms);
+    
 
     return (
         <div>
             <h1>Chat Room List Page</h1>
             <div>
-                {chatRooms.length > 0 ? (
+                {chatRooms?.length > 0 ? (
                     <ul>
                         {chatRooms.map(room => (
-                            <li key={room.id}>
-                                <Link href={`/chat/${room.id}`}>
-                                   {room.name}
+                            <li key={room?.id}>
+                                <Link href={`/chat/${room?.id}`}>
+                                   {room?.name}
                                 </Link>
                             </li>
                         ))}
---
+++ src/hooks/useChatRooms.jsx
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPubic from './useAxiosPublic';
+
+const useChatRooms = () => {
+    const axiosPublic = useAxiosPubic();
+    const { data: rooms = [] } = useQuery({
+        queryKey: ['rooms'],
+        queryFn: async () => {
+            const res = await axiosPublic.get("/rooms")
+            const data = await res.data;
+            return data;
+        }
+    })
+    return [rooms]
+};
+
+export default useChatRooms;
