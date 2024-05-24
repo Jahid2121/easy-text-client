4cc8355799c759b2897645979ee76951537ace87
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-05-24 18:15:16 +0600

    Create the layout of chatroom messages page

--- src/app/chat/[id]/page.jsx
+++ src/app/chat/[id]/page.jsx
@@ -1,13 +1,59 @@
+"use client"
 import React from 'react';
 
 const roomsDetailsPage = ({params}) => {
-    console.log(params.id);
+
+    const handleSendMessage = (message) => {
+        console.log('Message sent');
+    }
+    
     
     return (
-        <div>
-            Rooms Details page {params.id}
-        </div>
-    );
-};
+        <div className="max-w-4xl mx-auto p-6 bg-base-200 rounded-lg shadow-lg">
+      <h1 className="text-4xl font-bold text-center mb-6">Chat Room {params.id}</h1>
+      
+      <div className="mb-6">
+        {/* {messages.length > 0 ? (
+          <ul className="space-y-4">
+            {messages.map((message, index) => (
+              <li key={index} className="bg-base-100 p-4 rounded-lg">
+                {message}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-center text-gray-500 text-lg">No messages yet.</p>
+        )} */}
+
+<ul className="space-y-4">
+              <li className="bg-base-100 p-4 rounded-lg">
+                Hi there
+              </li>
+              <li className="bg-base-100 p-4 rounded-lg">
+                How are you doing ?
+              </li>
+              <li className="bg-base-100 p-4 rounded-lg">
+                Hi there
+              </li>
+              <li className="bg-base-100 p-4 rounded-lg">
+                Fine
+              </li>
+          </ul>
+      </div>
+      
+      <div className="flex space-x-4">
+        <input
+          type="text"
+          className="flex-grow input input-bordered"
+          placeholder="Type your message here..."
+
+        />
+        <button className="btn bg-green-500 hover:bg-green-700" onClick={handleSendMessage}>
+          Send
+        </button>
+      </div>
+    </div>
+        );
+    };
 
 export default roomsDetailsPage;
---
+++ src/components/ChatRoomList/ChatRoomList.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React from 'react';
+
+const ChatRoomList = ({rooms}) => {
+    return (
+        <div>
+        {rooms?.length > 0 ? (
+            <ul className='space-y-4'>
+                {rooms.map(room => (
+                    <li key={room?.id} className='bg-base-100 p-4 rounded-lg hover:bg-green-500 hover:text-white transition'>
+                        <Link  href={`/chat/${room?.id}`}>
+                           <h2 className="text-lg text-blue-500 hover:text-white">
+                           {room?.name}
+                           </h2>
+                           <p>{room?.description}</p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        ) : (
+            <p>No chat rooms available.</p>
+        )}
+    </div>
+    );
+};
+
+export default ChatRoomList;
